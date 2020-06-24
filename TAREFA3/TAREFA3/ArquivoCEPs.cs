ea8e79401c55d4d19f2a2f6b9295a8c7c52d97ed
magnussolidus <magnussolidus@gmail.com>
2020-06-24 20:37:12 -0300

    adição da tarefa 3
    adição do projeto para a tarefa 3, classe própria contendo a estrutura do arquivo CEPs.csv

---
+++ TAREFA3/TAREFA3/ArquivoCEPs.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAREFA3
+{
+    class ArquivoCEPs
+    {
+        public string CEP;
+        public string Logradouro;
+        public string Complemento;
+        public string Bairro;
+        public string Localidade;
+        public string UF;
+        public string Unidade;
+        public string IBGE;
+        public string GIA;
+
+        public ArquivoCEPs(string novoCep)
+        {
+            this.CEP = novoCep;
+        }
+
+        public void SetCEP(string valor)
+        {
+            CEP = valor;
+        }
+        public string GetCEP()
+        {
+            return CEP;
+        }
+        public void SetLogradouro(string valor)
+        {
+            Logradouro = valor;
+        }
+        public string GetLogradouro()
+        {
+            return Logradouro;
+        }
+        public void SetComplemento(string valor)
+        {
+            Complemento = valor;
+        }
+        public string GetComplemento()
+        {
+            return Complemento;
+        }
+        public void SetBairro(string valor)
+        {
+            Bairro = valor;
+        }
+        public string GetBairro()
+        {
+            return Bairro;
+        }
+        public void SetLocalidade(string valor)
+        {
+            Localidade = valor;
+        }
+        public string GetLocalidade()
+        {
+            return Localidade;
+        }
+        public void SetUF(string valor)
+        {
+            UF = valor;
+        }
+        public string GetUF()
+        {
+            return UF;
+        }
+        public void SetUnidade(string valor)
+        {
+            Unidade = valor;
+        }
+        public string GetUnidade()
+        {
+            return Unidade;
+        }
+        public void SetIBGE(string valor)
+        {
+            IBGE = valor;
+        }
+        public string GetIBGE()
+        {
+            return IBGE;
+        }
+        public void SetGIA(string valor)
+        {
+            GIA = valor;
+        }
+        public string GetGIA()
+        {
+            return GIA;
+        }
+    }
+}
---
+++ TAREFA3/TAREFA3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Preencha o endereco no arquivo "mapa.csv" lendo a coluna de cep e buscando na API viacep (https://viacep.com.br/) o endereço equivalente ao CEP e grave uma cópia do arquivo preenchido.
+ * */
+namespace TAREFA3
+{
+    class Program
+    {
+        const string ARQUIVO = "CEPs.csv"; // acredito que há um erro no enunciado da tarefa visto que o arquivo mapa.csv não contém coluna referente a CEP, assim utilizei o arquivo CEPs.csv
+        const string CAMINHO = "..\\..\\..\\..\\";
+        const char SEPARADOR = ';';
+        const string RESULTADO = "tarefa3.csv";
+
+        private static string _cabeçalho;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
