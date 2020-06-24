d74249c1d2a3315613fd236d1fc8deea0f45d676
magnussolidus <magnussolidus@gmail.com>
2020-06-24 19:19:00 -0300

    conclusão da tarefa
    tratamento dos dados, escrita do arquivo e liberação destes concluída.

--- TAREFA1/TAREFA1/Program.cs
+++ TAREFA1/TAREFA1/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 /*
@@ -15,18 +13,16 @@ namespace TAREFA1
 
         const int MULTIPLICADOR = 2;
         const string ARQUIVO = "mapa.csv";
-        const string CAMINHO = "..\\..\\..\\..\\" + ARQUIVO;
+        const string CAMINHO = "..\\..\\..\\..\\";
         const char SEPARADOR = ';';
       
 
         static void Main(string[] args)
         {
-            FileStream fonte = LerArquivo(CAMINHO);              
-
-
-            Console.WriteLine("Sucesso!");
+            FileStream fonte = LerArquivo(CAMINHO+ARQUIVO);
+            ProcessaDados(fonte);
+            Console.WriteLine("Arquivo 'tarefa1.csv' gerado com sucesso!");
             Console.ReadKey();
-
         }
 
         static FileStream LerArquivo(string fonte)
@@ -42,6 +38,11 @@ namespace TAREFA1
                 Console.ReadKey();
                 return null;
             }
+        }
+
+        static void FechaArquivo(FileStream alvo)
+        {
+            alvo.Close();
         }
 
         static void ProcessaDados(FileStream arquivo)
@@ -65,11 +66,29 @@ namespace TAREFA1
                 população.Add(int.Parse(valores[1]));
             }
 
+            FechaArquivo(arquivo);
+
             // processamento dos dados
 
+            List<int> resultado = new List<int>();
 
+            foreach (int pop in população)
+            {
+                resultado.Add(pop * MULTIPLICADOR);
+            }
 
             // escrita dos dados
+            
+            StreamWriter escritor = new StreamWriter(CAMINHO + "tarefa1.csv", false, Encoding.UTF8);
+            escritor.WriteLine(cabeçalho);
+
+            for(int i=0; i<cidade.Count; i++)
+            {
+                linha = cidade.ToArray()[i] + SEPARADOR + " " + resultado.ToArray()[i];
+                escritor.WriteLine(linha);
+            }
+
+            escritor.Close();
         }
     }
 }
