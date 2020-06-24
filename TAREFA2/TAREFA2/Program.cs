bd1fad1ed738905b589ce88607a149d0fd0f900e
magnussolidus <magnussolidus@gmail.com>
2020-06-24 19:21:55 -0300

    Criado solução para a tarefa 2
    projeto criado, descrição da tarefa adicionada ao arquivo

---
+++ TAREFA2/TAREFA2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 2 - Crie um programa que reordene o arquivo "mapa.csv" pela coluna "População no último censo" do arquivo, usando bubblesort, e grave uma cópia do arquivo alterado.
+ */
+namespace TAREFA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
