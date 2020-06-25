cb03f085c3dadc12d74051cbf3ac4db572299800
magnussolidus <magnussolidus@gmail.com>
2020-06-25 00:06:52 -0300

    tarefa 3 concluída
    adicionado tratamento assíncrono das chamadas, validação do cep ao criar o objeto, escrita do arquivo.

--- TAREFA3/TAREFA3/ArquivoCEPs.cs
+++ TAREFA3/TAREFA3/ArquivoCEPs.cs
@@ -20,7 +20,75 @@ namespace TAREFA3
 
         public ArquivoCEPs(string novoCep)
         {
-            this.CEP = novoCep;
+            this.CEP = AdjustCep(novoCep);
+        }
+
+        public ArquivoCEPs(string viacep, bool completo)
+        {
+            if(!completo)
+            {
+                return;
+            }
+
+            string[] dados = viacep.Split('|');
+            this.CEP = AdjustCep(dados[0]);
+            this.Logradouro = dados[1].Remove(0, 11);
+            this.Complemento = dados[2].Remove(0, 12);
+            this.Bairro = dados[3].Remove(0, 7);
+            this.Localidade = dados[4].Remove(0, 11);
+            this.UF = dados[5].Remove(0, 3);
+            this.Unidade = dados[6].Remove(0, 8);
+            this.IBGE = dados[7].Remove(0, 5);
+            this.GIA = dados[8].Remove(0, 4);
+        }
+
+        private string AdjustCep(string alvo)
+        {
+            bool removeu = false;
+
+            for(int i=0; i<alvo.Length; i++)
+            {
+                if(removeu)
+                {
+                    --i;
+                    removeu = false;
+                }
+                switch(alvo[i])
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        continue;
+                    default:
+                        alvo = alvo.Remove(i, 1);
+                        removeu = true;
+                        break;
+                        
+                }
+            }
+
+            if(alvo.Length < 8)
+            {
+                alvo = "ERRO! VERIFIQUE O CEP!";
+            }
+
+            return alvo;
+        }
+
+        public static bool IsValid(string CEP)
+        {
+            if(CEP == null || CEP.Contains("ERRO! VERIFIQUE O CEP!"))
+            {
+                return false;
+            }
+            return true;
         }
 
         public void SetCEP(string valor)
--- TAREFA3/TAREFA3/Program.cs
+++ TAREFA3/TAREFA3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,11 +16,126 @@ namespace TAREFA3
         const string CAMINHO = "..\\..\\..\\..\\";
         const char SEPARADOR = ';';
         const string RESULTADO = "tarefa3.csv";
-
+        
+        HttpClient client = new HttpClient();
         private static string _cabeçalho;
 
         static void Main(string[] args)
         {
+            FileStream fonte = LerArquivo(CAMINHO + ARQUIVO);
+            ArquivoCEPs[] dados =  ColetaDados(fonte);
+            
+            try
+            {
+                Task.WaitAll(ProcessaDados(dados));
+                
+                Console.WriteLine("Arquivo 'tarefa3.csv' gerado com sucesso!");
+                Console.ReadKey();
+
+            }
+            catch (AggregateException) { }
+
         }
+
+        static FileStream LerArquivo(string fonte)
+        {
+            try
+            {
+                FileStream resultado = new FileStream(fonte, FileMode.Open, FileAccess.Read);
+                return resultado;
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("\nErro ao encontrar o arquivo. Por favor verifique se o caminho e/ou o nome estão corretos.");
+                Console.ReadKey();
+                return null;
+            }
+        }
+
+        static void FechaArquivo(FileStream alvo)
+        {
+            alvo.Close();
+        }
+
+        static void SetCabeçalho(string valor)
+        {
+            _cabeçalho = valor;
+        }
+
+        static string GetCabeçalho()
+        {
+            return _cabeçalho;
+        }
+
+        static ArquivoCEPs[] ColetaDados(FileStream arquivo)
+        {
+            // coleta dos dados
+
+            StreamReader buffer = new StreamReader(arquivo);
+            List<ArquivoCEPs> dados = new List<ArquivoCEPs>();
+            string linha;
+
+            SetCabeçalho(buffer.ReadLine());
+
+            while ((linha = buffer.ReadLine()) != null)
+            {
+                var valores = linha.Split(SEPARADOR);
+                dados.Add(new ArquivoCEPs(valores[0]));
+            }
+
+            FechaArquivo(arquivo);
+            return dados.ToArray();
+        }
+
+        async static Task ProcessaDados(ArquivoCEPs[] dados)
+        {
+            // processamento dos dados
+
+            Program p = new Program();
+            int tamanho = dados.Length;
+            string atual;
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                if(!ArquivoCEPs.IsValid(dados[i].CEP))
+                {
+                    continue;
+                }
+                atual = await p.PegaViaCEP(dados[i].CEP);
+
+                dados[i] = new ArquivoCEPs(atual, true);
+            }
+
+            // escrita dos dados
+
+            StreamWriter escritor = new StreamWriter(CAMINHO + RESULTADO, false, Encoding.Default);
+            string linha;
+
+            escritor.WriteLine(_cabeçalho);
+
+            for (int i = 0; i < dados.Length; i++)
+            {
+                linha = dados[i].GetCEP() + SEPARADOR 
+                    + dados[i].GetLogradouro() + SEPARADOR
+                    + dados[i].GetComplemento() + SEPARADOR
+                    + dados[i].GetBairro() + SEPARADOR
+                    + dados[i].GetLocalidade() + SEPARADOR
+                    + dados[i].GetUF() + SEPARADOR
+                    + dados[i].GetUnidade() + SEPARADOR
+                    + dados[i].GetIBGE() + SEPARADOR
+                    + dados[i].GetGIA() + SEPARADOR;
+
+                escritor.WriteLine(linha);
+            }
+
+            escritor.Close();
+
+        }
+
+        private async Task<string> PegaViaCEP(string cep)
+        {
+            return await client.GetStringAsync("http://viacep.com.br/ws/"+cep+"/piped/");
+        }
+
     }
 }
